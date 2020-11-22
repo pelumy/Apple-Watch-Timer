cf37d567c443d6b79e0a67bbdc5541b8da7ebdc0
Itunu Raimi <chavitunu@gmail.com>
2020-11-22 10:45:32 +0100

    first commit

--- WatchTimerApp WatchKit Extension/InterfaceController.swift
+++ WatchTimerApp WatchKit Extension/InterfaceController.swift
@@ -11,16 +11,29 @@ import Foundation
 
 class InterfaceController: WKInterfaceController {
 
+    @IBOutlet weak var timer: WKInterfaceTimer!
+    
+    
     override func awake(withContext context: Any?) {
         // Configure interface objects here.
     }
     
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+        
     }
     
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
     }
-
+    
+    @IBAction func startTimer() {
+        timer.setDate(Date())
+        timer.start()
+    }
+    
+    @IBAction func stopTimer() {
+        timer.stop()
+    }
 }
