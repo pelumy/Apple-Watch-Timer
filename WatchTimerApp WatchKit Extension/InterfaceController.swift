dd21cd78eb6250418b90ac3c8829f7a371745a17
Itunu Raimi <chavitunu@gmail.com>
2020-11-22 11:17:02 +0100

    finished watch timer

--- WatchTimerApp WatchKit Extension/InterfaceController.swift
+++ WatchTimerApp WatchKit Extension/InterfaceController.swift
@@ -12,6 +12,11 @@ import Foundation
 class InterfaceController: WKInterfaceController {
 
     @IBOutlet weak var timer: WKInterfaceTimer!
+    @IBOutlet weak var timerActionButton: WKInterfaceButton!
+    
+    var isTimerStarted = false
+    var startTime = Date()
+    var elapsedTime: TimeInterval = 0.0
     
     
     override func awake(withContext context: Any?) {
@@ -28,12 +33,29 @@ class InterfaceController: WKInterfaceController {
         // This method is called when watch view controller is no longer visible
     }
     
-    @IBAction func startTimer() {
-        timer.setDate(Date())
-        timer.start()
+    
+    @IBAction func timerButtonAction() {
+        isTimerStarted = !isTimerStarted
+        if isTimerStarted {
+            startTime = Date()
+            timerActionButton.setTitle("STOP")
+            timer.setDate(Date(timeIntervalSinceNow: elapsedTime))
+            timer.start()
+        } else {
+            let stoppedTime = Date()
+            elapsedTime -= stoppedTime.timeIntervalSince(startTime)
+            timerActionButton.setTitle("START")
+            timer.stop()
+        }
     }
     
-    @IBAction func stopTimer() {
+    
+    @IBAction func resetTimer() {
+        isTimerStarted = false
+        startTime = Date()
+        timerActionButton.setTitle("START")
+        timer.setDate(startTime)
         timer.stop()
     }
+    
 }
